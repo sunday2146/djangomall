e14d56222597256f3109ad3bd52c44608e92ba3c
xingfu <2539909370@qq.com>
2021-10-15 09:56:40 +0800

    静态文件依赖本地化

--- DjangoMall/conf.py
+++ DjangoMall/conf.py
@@ -12,8 +12,8 @@ from DjangoMall.settings import alipay_private_key_path, alipay_public_key_path
 ################### 支付宝支付配置 #######################
 
 alipay_appid = "2021000116697536"
-return_url = "http://127.0.0.1:8000/order/alipay/"
-notify_url= "http://127.0.0.1:8000/order/alipay/"   # 可选, 不填则使用默认notify url
+return_url = "http://mall.lotdoc.cn/order/alipay/"
+notify_url= "http://mall.lotdoc.cn/order/alipay/"   # 可选, 不填则使用默认notify url
 alipay_debug=True,  # 默认False
 alipay_private_key_path = alipay_private_key_path
 alipay_public_key_path = alipay_public_key_path
